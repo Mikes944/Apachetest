f9d50114e1b1eaeca4fd867deca776d2c16f04e4
Mikes944 <144946289+Mikes944@users.noreply.github.com>
2023-11-30 16:24:46 +0000

    Update Index.js
    Remarked out all entries in this code so that it will not be run.  Will then move this to the gitignore page.
    Mkirby 30Nov2023

--- Index.js
+++ Index.js
@@ -7,35 +7,36 @@
 #include "http_protocol.h"
 #include "ap_config.h"
 
-static int helloworld_handler(request_rec *req)
-{
-    if (strcmp(req->handler, "helloworld")) {
-        return DECLINED;
-    }
-    if (!req->header_only) {
-        ap_rputs("{\"message\": \"Hello world!\"}\n", req);
-    }
-    req->content_type = "application/json;charset=UTF-8";
-
-    // ap_table_set is too old API
-    apr_table_set(req->headers_out, "X-Content-Type-Options", "nosniff");
-
-    return OK;
-}
-
-static void helloworld_register_hooks(apr_pool_t *p)
-{
-    printf("\n ** helloworld_register_hooks  **\n\n");
-    ap_hook_handler(helloworld_handler, NULL, NULL, APR_HOOK_MIDDLE);
-}
-
-/* Dispatch list for API hooks */
+#static int helloworld_handler(request_rec *req)
+#{
+#    if (strcmp(req->handler, "helloworld")) {
+#        return DECLINED;
+#    }
+#    if (!req->header_only) {
+#       ap_rputs("{\"message\": \"Hello world!\"}\n", req);
+#    }
+#    req->content_type = "application/json;charset=UTF-8";#
+#
+#    // ap_table_set is too old API
+#    apr_table_set(req->headers_out, "X-Content-Type-Options", "nosniff");
+#
+#    return OK;
+#}
+#
+#static void helloworld_register_hooks(apr_pool_t *p)
+#{
+#    printf("\n ** helloworld_register_hooks  **\n\n");
+#    ap_hook_handler(helloworld_handler, NULL, NULL, APR_HOOK_MIDDLE);
+#}
+#
+#/* Dispatch list for API hooks */
 module AP_MODULE_DECLARE_DATA helloworld_module = {
-    STANDARD20_MODULE_STUFF, 
-    NULL, /* create per-dir    config structures */
-    NULL, /* merge  per-dir    config structures */
-    NULL, /* create per-server config structures */
-    NULL, /* merge  per-server config structures */
-    NULL, /* table of config file commands       */
-    helloworld_register_hooks  /* register hooks */
-};
+#    STANDARD20_MODULE_STUFF, 
+#    NULL, /* create per-dir    config structures */
+#    NULL, /* merge  per-dir    config structures */
+#    NULL, /* create per-server config structures */
+#    NULL, /* merge  per-server config structures */
+#    NULL, /* table of config file commands       */
+#    helloworld_register_hooks  /* register hooks */
+#};#
+#
